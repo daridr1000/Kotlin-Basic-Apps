832ed879f5139573563a3184b9ad1334c9017cd4
Dragnea Darius <ddariusmihai@gmail.com>
2021-07-19 16:18:30 +0300

    some apps made with kotlin in android studio

---
+++ DiceRoller/app/src/main/java/org/meicode/diceroller/MainActivity.kt
@@ -0,0 +1,51 @@
+package org.meicode.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice(diceImage)
+            rollDice(diceImage2)
+        }
+
+        rollDice(diceImage)
+        rollDice(diceImage2)
+    }
+
+    private fun rollDice(image : ImageView) {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+
+
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        image.setImageResource(drawableResource)
+        image.contentDescription = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
