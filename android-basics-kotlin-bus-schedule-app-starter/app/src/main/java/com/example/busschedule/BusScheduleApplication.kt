e42230d9f480f8c7bdf8c2e24bad84aec57dc1c1
Dragnea Darius <ddariusmihai@gmail.com>
2021-07-27 10:43:07 +0300

    learned how to manipulate databases within android apps

---
+++ android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/BusScheduleApplication.kt
@@ -0,0 +1,7 @@
+package com.example.busschedule
+
+import com.example.busschedule.database.AppDatabase
+import android.app.Application
+class BusScheduleApplication : Application() {
+    val database: AppDatabase by lazy { AppDatabase.getDatabase(this) }
+}
